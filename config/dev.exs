75f4193637cdc263be9985683cbdb86382a779ce
marc@roosoft.com <marc@roosoft.com>
2023-01-31 14:11:16 -0500

    moved the dev database in the priv/db folder

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ import Config
 
 # Configure your database
 config :nostr_poc, NostrPoc.Repo,
-  database: Path.expand("../nostr_poc_dev.db", Path.dirname(__ENV__.file)),
+  database: Path.expand("../priv/db/nostr_poc_dev.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
